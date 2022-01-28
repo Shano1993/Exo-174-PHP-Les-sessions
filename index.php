7c15ce2002e16191084721afa454a3ba7b3d5dc3
Shano1993 <stefan.hanotiau@gmail.com>
2022-01-28 11:24:58 +0100

    php

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 
 // Premiere ligne
 
+session_start();
+$_SESSION['couleur'] = 'rouge';
+echo "La valeur session 'couleur' a été définie !";
+?>
+
+<a href="index2.php">Voir la valeur de la session</a>
+
 
 
 
